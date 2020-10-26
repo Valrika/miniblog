6c6a1dc44cd725b761d74e9235e11e83f349dbea
ValerieEkoume <72264813+ValerieEkoume@users.noreply.github.com>
2020-10-26 14:29:27 +0100

    test2

--- home.php
+++ home.php
@@ -4,3 +4,58 @@
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <title>Beauté O Naturel</title>
+    <meta charset="UFT-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--css bootstrap-->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+
+<header>
+    <div class="navbar-header"></div>
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">Accueil</a>
+            <a class="navbar-brand" href="#">Articles</a>
+            <a class="navbar-brand" href="#">A propos</a>
+            <a class="navbar-brand" href="#">Contact</a>
+
+        </nav>
+    </div>
+    <!-- insertion de la bannière dans la div-->
+    <div class="container-fluid"><img src="images/banniere.png" class="img-fluid" alt="responsive image">
+
+        <!-- création d'un boutton menu pour la navigation mobile
+        <button class="navbar-toggler" type="button"  data-toggle="collapse" data-target="#MyNavbar"></button>-->
+
+    </div>
+</header>
+
+<body>
+<div class="container"></div>
+<div class="d-flex p-2">
+    <div class="col-md-4">
+        <h3>Les joies de la méditation<h3>
+                <img src="images/sable.jpg" class="img-fluid" alt="sable fin et galets">
+                <p>What is Lorem Ipsum?
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+    </div>
+    <div class="col-md-4">
+        UN PEU
+    </div>
+    <div class="col-md-4">
+        BEAUCOUP
+    </div><br>
+</div>
+
+
+</body>
+
+</html>
