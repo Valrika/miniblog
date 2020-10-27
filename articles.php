29bdb60216adf649d8c42b62c2b56d4316267a31
ValerieEkoume <72264813+ValerieEkoume@users.noreply.github.com>
2020-10-27 09:06:43 +0100

    modif add

---
+++ articles.php
@@ -0,0 +1,52 @@
+<?php
+
+//page pour créer un article
+
+
+?>
+<html lang="fr">
+<head>
+    <title>Beaute O Naturel</title>
+    <meta charset="UFT-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--css bootstrap-->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<header>
+    <div class="navbar-header "></div>
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="home.php">Accueil</a>
+            <a class="navbar-brand" href="articles.php">Articles</a>
+            <a class="navbar-brand" href="#">A propos</a>
+            <a class="navbar-brand" href="#">Contact</a>
+            <a class="navbar-brand" href="create_article.php">créer un article</a>
+
+        </nav>
+    </div>
+
+</header>
+
+<br>
+<br>
+
+<body>
+<div
+
+<h6 class="text-center">Sport</h6><!--titre de la catégorie-->
+<h3 class="text-center">LES BIENFAITS DE LA MARCHE A PIEDS</h3><br>
+<br>
+
+<h6 class="text-center">26 octobre 2020</h6><!--date de création à mettre en place php-->
+<div class="d-flex justify-content-center">
+    <img src="images/marche.jpg" class="align-content-center" alt="galets en forme de pieds" width="50%">
+</div>
+
+
+
+
+</body>
+
+</html>
--- home.php
+++ home.php
@@ -20,12 +20,13 @@
 
 <header>
     <div class="navbar-header"></div>
-    <div class="container">
+    <div class="d-flex justify-content-center">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Accueil</a>
-            <a class="navbar-brand" href="#">Articles</a>
+            <a class="navbar-brand" href="home.php">Accueil</a>
+            <a class="navbar-brand" href="articles.php">Articles</a>
             <a class="navbar-brand" href="#">A propos</a>
             <a class="navbar-brand" href="#">Contact</a>
+            <a class="navbar-brand" href="create_article.php">créer un article</a>
 
         </nav>
     </div>
@@ -42,16 +43,12 @@
 <div class="container"></div>
 <div class="d-flex p-2">
     <div class="col-md-4">
-        <h3>Les joies de la méditation<h3>
-                <img src="images/sable.jpg" class="img-fluid" alt="sable fin et galets">
-                <p>What is Lorem Ipsum?
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+
+    <div class="col-md-4">
+
     </div>
     <div class="col-md-4">
-        UN PEU
-    </div>
-    <div class="col-md-4">
-        BEAUCOUP
+
     </div><br>
 </div>
 
