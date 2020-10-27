83706635cb7c86f410b606ea7c01660b446ff685
ValerieEkoume <72264813+ValerieEkoume@users.noreply.github.com>
2020-10-27 11:49:36 +0100

    new

--- create_article.php
+++ create_article.php
@@ -52,7 +52,7 @@ $statement->execute($insert_data);
 
 //dans le title entre deux balises php
 
-echo $slug,
+echo $slug;
 
 
 ?>
@@ -65,22 +65,67 @@ echo $slug,
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
-
+<body>
 <header>
-    <div class="navbar-header "></div>
-    <div class="container">
+    <div class="navbar-header"></div>
+    <div class="d-flex justify-content-center">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand" href="home.php">Accueil</a>
-            <a class="navbar-brand" href="articles.php">Articles</a>
-            <a class="navbar-brand" href="#">A propos</a>
-            <a class="navbar-brand" href="#">Contact</a>
-            <a class="navbar-brand" href="create_article.php">créer un article</a>
+            <a class="navbar-brand" href="articles.php">Sport</a>
+            <a class="navbar-brand" href="#">Nutrition</a>
+            <a class="navbar-brand" href="#">Mompreneuse</a>
+            <a class="navbar-brand" href="#">About</a>
+            <a class="navbar-brand" href="create_article.php">Créer un article</a>
 
         </nav>
     </div>
+<br>
+    <br>
+
 
 </header>
 
+<form>
+    <div class="form-row">
+        <div class="col">
+            <input type="text" class="form-control" placeholder="First name">
+        </div>
+        <div class="col">
+            <input type="text" class="form-control" placeholder="Last name">
+        </div>
+    </div>
+</form>
 
+<div class="col-md-3 mb-3">
+    <label for="validationCustom04">Catégories</label>
+    <select class="custom-select" id="validationCustom04" required>
+        <option selected disabled value="">Choisir...</option>
+        <option>Sport</option>
+        <option>Nutrition</option>
+        <option>Mompreneuse</option>
+    </select>
+    <div class="invalid-feedback">
+        Please select a valid state.
+    </div>
+</div>
+<div class="col-md-3 mb-3">
+    <label for="validationCustom05">Date</label>
+    <input type="text" class="form-control" id="validationCustom05" required>
+    <div class="invalid-feedback">
+        Please provide a valid zip.
+    </div>
+</div>
+    <div class="form-group">
+        <label for="exampleFormControlTextarea1">Example textarea</label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    </div>
+<button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+<footer>
+
+</footer>
+
+</body>
 
 </html>
--- footer.php
+++ footer.php
@@ -1,5 +1,35 @@
-<?php 
+ <?php
 
 //footer à afficher sur toutes les pages
 
 ?>
+
+ <!DOCTYPE html>
+ <html lang="fr">
+
+ <head>
+     <title>Beauté O Naturel</title>
+     <meta charset="UFT-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <!--css bootstrap-->
+     <link rel="stylesheet" href="css/bootstrap.min.css">
+     <link rel="stylesheet" href="css/style.css">
+ </head>
+
+ <body>
+
+        <div class="footer"></div>
+        <!-- Le footer a 2 parties celle du haut = content-->
+            <div class="footer-content"></div>
+                <div class="footer-section about"></div>
+                <div class="footer-section links"></div>
+                <div class="footer-section contact form"></div>
+
+        <!-- Le footer a 2 parties celle du bas = bottom-->
+            <div class="footer-bottom"></div>
+
+
+
+
+
+ </body>
