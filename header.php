421d0995ab6e612189fa5390b788b64908dd9f97
mvalma <66174692+mvalma@users.noreply.github.com>
2020-10-29 16:02:48 +0100

    changes

--- header.php
+++ header.php
@@ -1,3 +1,14 @@
+<html lang="fr">
+    <head>
+        <title>Beaute O Naturel</title>
+        <meta charset="UFT-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!--css bootstrap-->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/style.css">
+        <script src="https://kit.fontawesome.com/31cfd28a45.js" crossorigin="anonymous"></script>
+    </head>
+<body>
 <header>
     <div class="navbar-header"></div>
     <div class="d-flex justify-content-center">
@@ -8,8 +19,13 @@
             <a class="navbar-brand" href="#">Mompreneuse</a>
             <a class="navbar-brand" href="#">About</a>
             <a class="navbar-brand" href="create_article.php">Créer un article</a>
-
         </nav>
     </div>
-
 </header>
+
+<?php
+$pdo = new PDO('mysql:host=mysql;dbname=miniblog;host=127.0.0.1', 'root', '', [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+
+?>
