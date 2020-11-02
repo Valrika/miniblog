be72e653c2cac86339c7029d1a715beecb2947f1
ValerieEkoume <72264813+ValerieEkoume@users.noreply.github.com>
2020-11-02 17:14:43 +0100

    new file

--- home.php
+++ home.php
@@ -2,7 +2,6 @@
 
 // page d'accueil
 
-
 ?>
 
 <!DOCTYPE html>
@@ -18,31 +17,55 @@
     <!-- Fontawesome -->
 
     <script src="https://kit.fontawesome.com/31cfd28a45.js" crossorigin="anonymous"></script>
+    <!-- jQuery and JS bundle w/ Popper.js -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/31cfd28a45.js" crossorigin="anonymous"></script>
 </head>
 
+
 <body>
+
 <header>
-<!-- Barre de navigation -->
-    <div class="navbar-header"></div>
-    <div class="navbar-collapse">
 
-    <div class="d-flex justify-content-center">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="home.php">ACCUEIL</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-        <nav class="navbar navbar-expand-lg">
-            <a class="navbar-brand" href="home.php">Accueil</a>
-            <a class="navbar-brand" href="articles.php">Articles</a>
-            <a class="navbar-brand" href="footer.html">A Propos</a>
-            <a class="navbar-brand" href="create_article.php">Créer un article</a>
-
-        </nav>
-    </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="articles.php">ARTICLES<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        CATEGORIES
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Sport</a>
+                        <a class="dropdown-item" href="#">Nutrition</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Mompreneuse</a>
+                    </div>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">A PROPOS<span class="sr-only">(current)</span></a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Connectez-vous</button>
+            </form>
+        </div>
+    </nav>
+    </header>
 
 
 <!-- insertion de la bannière -->
     <div class="banniere">
         <img src="images/banniere.png" class="img-fluid" alt="responsive image">
     </div>
-</header>
 
 <br>
 <br>
@@ -75,7 +98,7 @@
                 <img src="images/workingmom2.jpg" alt="" class="slider-image">
                 <div class="post-info">
                     <h5><a href="articles.php">Maman à temps plein</a></h5>
-                    <i class="fa fa-user">Valérie EKOUME</i>
+                    <i class="fa fa-user">Marika VALMALETTE</i>
                     &nbsp;
                     <i class="fa fa-calendar">Lundi 26 octobre</i>
                 </div>
@@ -85,7 +108,7 @@
                 <img src="images/bananas.jpg" alt="" class="slider-image">
                 <div class="post-info">
                     <h5><a href="articles.php">Bananas !</a></h5>
-                    <i class="fa fa-user">Valérie EKOUME</i>
+                    <i class="fa fa-user">Marika VALMALETTE</i>
                     &nbsp;
                     <i class="fa fa-calendar">Lundi 26 octobre</i>
                 </div>
@@ -104,7 +127,7 @@
             <div class="post">
                 <img src="images/drink.jpg" alt="" class="slider-image">
                 <div class="post-info">
-                    <h5><a href="articles.php">Besoin d'une pose</a></h5>
+                    <h5><a href="articles.php">Besoin d'une pose?</a></h5>
                     <i class="fa fa-user">Valérie EKOUME</i>
                     &nbsp;
                     <i class="fa fa-calendar">Lundi 26 octobre</i>
@@ -154,7 +177,7 @@
 
             <!-- colonne 2 liens vers les CATEGORIES du site-->
             <div class="col-md-3 segment-two md-mb-30 md-mb-30">
-                    <h2>Belle O Naturel</h2>
+                    <h2>CATEGORIES</h2>
                     <ul>
                         <li><a href="#">Sport</a></li>
                         <li><a href="#">Nutrition</a></li>
---
+++ log_in.php
@@ -0,0 +1,41 @@
+<?php
+
+?>
+<html lang="fr">
+    <head>
+        <title>Beaute O Naturel</title>
+        <meta charset="UFT-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!--css bootstrap-->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/style.css">
+
+
+    <div class="titr">
+        <h2 class="display-4">LOG IN</h2>
+    </div>
+
+    <form class="log-in">
+        <div class="form-group col-md-6">
+            <label for="exampleInputEmail1">Adresse mail</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-group col-md-6">
+            <label for="exampleInputPassword1">Mot de passe</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+
+            <div class="form-group col-md-12" style="text-align: center">
+                <button type="submit" class="btn">Envoyer</button>
+            </div>
+    </form>
+
+
+<html>
+
+
+
+
+
+
