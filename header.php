6049341b93ad710165250b226194372ed2484baa
mvalma <66174692+mvalma@users.noreply.github.com>
2020-11-03 15:42:13 +0100

    changements slug, requete sql et message validation

--- header.php
+++ header.php
@@ -20,40 +20,41 @@
 
 <header>
 
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<a class="navbar-brand" href="home.php">ACCUEIL</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span>
+</button>
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="home.php">ACCUEIL</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+<ul class="navbar-nav mr-auto">
+<li class="nav-item active">
+<a class="nav-link" href="articles.php">ARTICLES<span class="sr-only">(current)</span></a>
+</li>
+<li class="nav-item dropdown">
+<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+CATEGORIES
+</a>
+<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+<a class="dropdown-item" href="#">Sport</a>
+<a class="dropdown-item" href="#">Nutrition</a>
+<div class="dropdown-divider"></div>
+<a class="dropdown-item" href="#">Mompreneuse</a>
+</div>
+</li>
+<li class="nav-item active">
+<a class="nav-link" href="create_article.php">CREER UN ARTICLE<span class="sr-only">(current)</span></a>
+</li>
+<li class="nav-item active">
+<a class="nav-link" href="#">A PROPOS<span class="sr-only">(current)</span></a>
+</li>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="articles.php">ARTICLES<span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        CATEGORIES
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Sport</a>
-                        <a class="dropdown-item" href="#">Nutrition</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Mompreneuse</a>
-                    </div>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="about.php">A PROPOS<span class="sr-only">(current)</span></a>
-                </li>
-            </ul>
-            <form class="form-inline my-2 my-lg-0">
-                <div class="loginbtn">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit"><a href="log_in.php">Connectez-vous</a></button>
-                </div>
-            </form>
-        </div>
-    </nav>
+</ul>
+<form class="form-inline my-2 my-lg-0">
+<button class="btn btn-outline-success my-2 my-sm-0" type="submit"><a href="log_in.php">Connectez-vous</button></a>
+</form>
+</div>
+</nav>
 </header>
 
 
@@ -62,6 +63,20 @@
 $pdo = new PDO('mysql:host=mysql;dbname=miniblog;host=127.0.0.1', 'root', '', [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]);
+
+function slugify($string, $delimiter = '-')
+    {
+        $re = '/(\?\!\'\")*/';
+        $oldLocale = setlocale(LC_ALL, '0');
+        setlocale(LC_ALL, 'en_US.UTF-8');
+        $clean = iconv('UTF-8', 'ASCII//TRANSLIT', $string);
+        $clean = preg_replace("/[^a-zA-Z0-9\/_|+ -]/", '', $clean);
+        $clean = strtolower($clean);
+        $clean = preg_replace($re, $delimiter, $clean);
+        $clean = trim($clean, $delimiter);
+        setlocale(LC_ALL, $oldLocale);
+        return $clean;
+    }
 
 ?>
 
